9b749c612264cc619b132015b280b6bf948c1e42
mrmrkay <manhtran2198@gmail.com>
2019-04-07 11:56:16 +0700

    push

--- QLThuVien/Form1.Designer.cs
+++ QLThuVien/Form1.Designer.cs
@@ -39,6 +39,7 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.panelControls = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -114,6 +115,7 @@
             this.button5.Size = new System.Drawing.Size(75, 67);
             this.button5.TabIndex = 4;
             this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button4
             // 
@@ -124,6 +126,7 @@
             this.button4.Size = new System.Drawing.Size(65, 67);
             this.button4.TabIndex = 3;
             this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button3
             // 
@@ -134,6 +137,7 @@
             this.button3.Size = new System.Drawing.Size(75, 67);
             this.button3.TabIndex = 2;
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
@@ -144,6 +148,7 @@
             this.button2.Size = new System.Drawing.Size(79, 67);
             this.button2.TabIndex = 1;
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
@@ -156,14 +161,24 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // panelControls
+            // 
+            this.panelControls.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelControls.Location = new System.Drawing.Point(0, 123);
+            this.panelControls.Name = "panelControls";
+            this.panelControls.Size = new System.Drawing.Size(1141, 425);
+            this.panelControls.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1141, 516);
+            this.ClientSize = new System.Drawing.Size(1141, 548);
+            this.Controls.Add(this.panelControls);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -183,6 +198,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panelControls;
     }
 }
 
--- QLThuVien/Form1.cs
+++ QLThuVien/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using QLThuVien.UserControlls;
 
 namespace QLThuVien
 {
@@ -17,9 +18,47 @@ namespace QLThuVien
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void AddControlsToPanel(UserControl c)
+        {
+            c.Dock = DockStyle.Fill;
+            panelControls.Controls.Clear();
+            panelControls.Controls.Add(c);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ucBook ucbook = new ucBook();
+            AddControlsToPanel(ucbook);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ucBorrow ucborrow = new ucBorrow();
+            AddControlsToPanel(ucborrow);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ucSearch ucsearch = new ucSearch();
+            AddControlsToPanel(ucsearch);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ucAccount ucac = new ucAccount();
+            AddControlsToPanel(ucac);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ucHelp uchelp = new ucHelp();
+            AddControlsToPanel(uchelp);
         }
         
     }
