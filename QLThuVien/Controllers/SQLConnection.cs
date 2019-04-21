49c5068890c8e2007b5a5460d2304433b78d661c
mrmrkay <manhtran2198@gmail.com>
2019-04-21 21:41:20 +0700

    a

--- QLThuVien/Controllers/SQLConnection.cs
+++ QLThuVien/Controllers/SQLConnection.cs
@@ -19,7 +19,7 @@ namespace QLKho.Controller
 
         private SqlConnect() { }
 
-        public static string connectionString = @"Data Source=MRMRKAY\SQLEXPRESS;Initial Catalog=QLKHO;Integrated Security=True";
+        public static string connectionString = @"Data Source=MRMRKAY\SQLEXPRESS;Initial Catalog=QLThuVien;Integrated Security=True";
 
         public DataTable ExecuteQuery(string query)
         {
